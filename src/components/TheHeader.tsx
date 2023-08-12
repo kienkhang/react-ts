ee93278ab3f199165814ff16ed5e849acd41fd9f
NNam2411 <nguyennam2002ro@gmail.com>
2023-08-12 12:51:18 +0700

    Fix: Fixed header
    Added Download CV button
    Fixed border

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,8 +1,8 @@
 import { useDisclosure } from '@chakra-ui/react'
 import SharedDrawer from './shared/SharedDrawer'
-import SharedSwitchTheme from './shared/SharedSwitchTheme'
-
+import useTheme from '@/hooks/useTheme'
 const TheHeader = () => {
+  const { localTheme: theme, selectDark, selectLight, selectSystem } = useTheme({})
   const { isOpen: visible, onOpen: open, onClose: close } = useDisclosure()
 
   return (
@@ -15,30 +15,48 @@ const TheHeader = () => {
         </div>
         <div className="hidden xl:block">
           <ul className="flex space-x-6">
-            <li>
-              <Link to="/about" className="text-[##4B5563] hover:underline">
+            <li className="flex justify-center items-center">
+              <Link to="/about" className=" text-[##4B5563] hover:underline">
                 About
               </Link>
             </li>
-            <li>
+            <li className="flex justify-center items-center">
               <Link to="/work" className="text-[##4B5563] hover:underline">
                 Work
               </Link>
             </li>
-            <li>
+            <li className="flex justify-center items-center">
               <Link to="/terminals" className="text-[##4B5563] hover:underline">
-                Terminals
+                Testimonials
               </Link>
             </li>
-            <li>
+            <li className="flex justify-center items-center">
               <Link to="/contact" className="text-[##4B5563] hover:underline mr-6">
                 Contact
               </Link>
             </li>
-            <li>
-              {/* Switch Theme */}
-              <SharedSwitchTheme></SharedSwitchTheme>
+            <div className="w-[1px] h-6 bg-[#F3F4F6] dark:bg-[#1F2937] self-center"></div>
+            <li className="flex justify-center items-center">
+              <div className="flex dark:hidden justify-center p-[6px]">
+                <ILocalSun
+                  className={`h-6 w-6 cursor-pointer select-none ${
+                    theme === 'light' ? 'text-orange-500' : ''
+                  }`}
+                  onClick={selectDark}
+                />
+              </div>
+              <div className="hidden dark:flex justify-center p-[6px]">
+                <ILocalMoon
+                  className={`w-6 h-6 hidden dark:inline-block cursor-pointer select-none ${
+                    theme === 'dark' ? 'text-yellow-500' : ''
+                  }`}
+                  onClick={selectLight}
+                />
+              </div>
             </li>
+            <button className="bg-[#111827] dark:bg-[#F9FAFB] text-[#F9FAFB] dark:text-[#111827] rounded-xl py-[6px] px-4">
+              <h1 className="w-[104px] h-6 flex items-center justify-center">Download CV</h1>
+            </button>
           </ul>
         </div>
       </div>
--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -4,7 +4,7 @@ const SkillsSection = () => {
   return (
     <div className="px-4 xl:px-20 py-4 xl:py-[96px] items-stretch">
       <div className="flex flex-col items-center xl:px-4">
-        <div className="rounded-xl text-sm bg-[#E5E7EB] w-fit px-5 py-1 mb-4">
+        <div className="rounded-xl text-sm text-[#4B5563] dark:text-[#D1D5DB] bg-[#E5E7EB] dark:bg-[#374151] w-fit px-5 py-1 mb-4">
           <h1 className="text-center mx-auto">Skills</h1>
         </div>
         <h2 className="w-full text-center">
