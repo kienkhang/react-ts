2fd437cd2756fb1e1d85dcb3a0fbbd5bdfbbd40e
Sirenef <anhngocbui332@gmail.com>
2023-08-14 19:07:48 +0700

    feat/ add aboutme section

---
+++ src/components/aboutme/AboutMePhoto.tsx
@@ -0,0 +1,4 @@
+const AboutMePhoto = () => {
+  return <div className="h-[380px] xl:h-[694px]   bg-blue-400 "></div>
+}
+export default AboutMePhoto
--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -5,12 +5,12 @@ const SkillsSection = () => {
     <div className="px-4 xl:px-20 py-4 xl:py-[96px] items-stretch">
       <div className="flex flex-col items-center xl:px-4">
         <div className="rounded-xl text-sm text-[#4B5563] dark:text-[#D1D5DB] bg-[#E5E7EB] dark:bg-[#374151] w-fit px-5 py-1 mb-4">
-          <h1 className="text-center mx-auto">Skills</h1>
+          <h1 className="mx-auto text-center">Skills</h1>
         </div>
         <h2 className="w-full text-center">
           The skills, tools and technologies I am really good at:
         </h2>
-        <div className="flex flex-col xl:flex-row xl:justify-center mt-8 xl:mt-12">
+        <div className="flex flex-col mt-8 xl:flex-row xl:justify-center xl:mt-12">
           <SkillsDetails></SkillsDetails>
         </div>
       </div>
