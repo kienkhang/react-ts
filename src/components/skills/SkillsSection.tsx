71203c5045c11d018bf6f507f54c1bd0008291ad
NNam2411 <nguyennam2002ro@gmail.com>
2023-08-15 00:15:11 +0700

    fix: icon section

--- src/components/skills/SkillsSection.tsx
+++ src/components/skills/SkillsSection.tsx
@@ -3,7 +3,7 @@ import SkillsDetails from './SkillsDetails'
 const SkillsSection = () => {
   return (
     <div className="px-4 xl:px-20 py-4 xl:py-[96px] items-stretch">
-      <div className="flex flex-col items-center xl:px-4">
+      <div className="flex flex-col items-center xl:px-8">
         <div className="rounded-xl text-sm text-[#4B5563] dark:text-[#D1D5DB] bg-[#E5E7EB] dark:bg-[#374151] w-fit px-5 py-1 mb-4">
           <h1 className="mx-auto text-center">Skills</h1>
         </div>
