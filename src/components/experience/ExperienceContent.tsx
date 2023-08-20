657c2a7caba617580a328afe2d981c274e3ad5d4
anh-whatstrack <anh.whatstrack@gmail.com>
2023-08-20 21:08:54 +0700

    feat: add format date

--- src/components/experience/ExperienceContent.tsx
+++ src/components/experience/ExperienceContent.tsx
@@ -1,10 +1,12 @@
+import dayjs from 'dayjs'
 type ContentProps = {
-  date: string
+  startTime: string
+  endTime: string
   title: string
   content: string
 }
 
-const ExperienceContent = ({ date, title, content }: ContentProps) => {
+const ExperienceContent = ({ startTime, endTime, title, content }: ContentProps) => {
   const contentArray = content.split('.')
   return (
     <div className=" p-8 flex flex-col items-start  bg-white rounded-xl shadow-md gap-4 dark:bg-[#1F2937] dark:shadow-2xl xl:grid xl:grid-flow-col xl:gap-12 max-w-[832px] ">
@@ -12,7 +14,13 @@ const ExperienceContent = ({ date, title, content }: ContentProps) => {
         <ILocalUpwork className=" w-[102px] h-7"></ILocalUpwork>
       </div>
       <div className=" flex flex-col gap-4 xl:flex-row-reverse xl:gap-12 ">
-        <div className=" text-base font-normal text-[#374151] dark:text-[#E5E7EB]">{date}</div>
+        <div className=" text-base font-normal text-[#374151] dark:text-[#E5E7EB]">
+          {dayjs(startTime).format('MMM YYYY') +
+            ' - ' +
+            (dayjs(endTime).format('MMM YYYY') != 'Invalid Date'
+              ? dayjs(endTime).format('MMM YYYY')
+              : 'Present')}
+        </div>
         <div className="">
           <div className=" text-lg font-semibold text-[#111827] dark:text-white ">{title}</div>
           <div className=" text-base font-normal text-[#4B5563] self-stretch">
